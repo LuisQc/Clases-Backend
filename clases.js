3abb5cfa433cfea317401e9c3dbce95f370cc145
LuisQc <pqr.24.08.200@gmail.com>
2022-07-05 01:07:19 -0500

    metodos

--- clases.js
+++ clases.js
@@ -1,25 +1,30 @@
 class Usuario {
-    constructor(nombre, apellido,libros, mascotas){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.libros = libros;
-        this.mascotas = [];
-    }
+  constructor(nombre, apellido, libros, mascotas) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.LibroNombre = LibroNombre;
+    this.LibroAutor = LibroAutor;
+    this.mascotas = [];
+  }
 
-getFullName() {
-   console.log(`Hola ${this.nombre} como estas`);
-   
-}
-addMascota(mascota) {
-    this.mascotas.push(mascota);
-}
+// APLICANDO METODO DE NOMBRE COMPLETO
+  getFullName() {
+    console.log(`Hola ${this.nombre} ${this.apellido} como estas`);
+  }
 
-countMascota() {
-    console.log(this.mascotas.length);
+  addMascota(mascota) {
+    this.mascotas.push(mascota)
+  }
+  countMascotas() {
+    console.log(this.mascotas.length)
+  }
+  addBook(){
+    console.log(JSON.parse(this.LibroNombre,this.LibroAutor));
+  }
+
 
 }
-// addBook(nombre,autor) {
-
-// }
-// getBookNames() {
-}
+const a = new Usuario("Homer" , "Odisea");
+    a.addBook();
+const p = new Usuario("jorge", "Smith");
+    p.getFullName();
